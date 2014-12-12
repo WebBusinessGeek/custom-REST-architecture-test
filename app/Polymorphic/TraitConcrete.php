c7d3f27fd5946b17bdcdaf92fc43bfe8f05f951f
Kevin Washington <kevw12188@gmail.com>
2014-12-12 12:45:53 -0500

    added prep for tdd

--- app/Polymorphic/TraitConcrete.php
+++ app/Polymorphic/TraitConcrete.php
@@ -3,17 +3,15 @@
  * Created by PhpStorm.
  * User: MacBookEr
  * Date: 12/12/14
- * Time: 12:18 PM
+ * Time: 12:43 PM
  */
 
 namespace App\Polymorphic;
 
 
-class TraitTester {
+class TraitConcrete {
 
     use AuthorizationTrait, AuthorizationTrait, FactoryTrait,
         InvokerTrait, RepositoryTrait, ResponderTrait, ValidatorTrait;
-
-
 
 }
---
+++ tests/Polymorphic/TraitConcreteTest.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: MacBookEr
+ * Date: 12/12/14
+ * Time: 12:44 PM
+ */
+
+use \Illuminate\Foundation\Testing\TestCase;
+
+class TraitConcreteTest extends \TestCase {
+
+}
